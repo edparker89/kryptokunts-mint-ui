e286ed2cf261dad1491616547a6149906712fc9c
AlexCPT-cpu <alexodey79@gmail.com>
2024-11-27 11:57:02 +0100

    deploy to vercel

---
+++ settings.tsx
@@ -0,0 +1,35 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+  // {
+  //   label: "WL",
+  //   mintText: "WL mint paying with SOL",
+  //   buttonLabel: "Mint now!",
+  //   header: "WL Mint",
+  // },
+  // {
+  //   label: "OG",
+  //   mintText: "Mint Paying with ABC",
+  //   buttonLabel: "Mint now!",
+  //   header: "OG Mint",
+  // },
+  {
+    label: "public",
+    mintText: "Mint Paying with DEF",
+    buttonLabel: "Mint now!",
+    header: "Public Mint",
+  },
+  {
+    label: "default",
+    mintText: "default mintText",
+    buttonLabel: "Mint now!",
+    header: "Default",
+  },
+];
+
+//header image in the ui. replace with your own
+export const image =
+  "https://peach-smoggy-opossum-618.mypinata.cloud/ipfs/QmVZ2D1ZqStnQhpub5jEUAKftdkb5a7GUbqWXVzU8p9Eqd/0.png";
+
+//website title
+export const headerText = "Test NFT";
